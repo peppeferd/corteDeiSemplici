adca1333972fe5428b3989af3112ce0c793f098e
peppeferd <giuseppeferlazzo@outlook.it>
2025-08-27 09:57:37 +0200

    Commit

--- src/components/BannerCelebrazioni.jsx
+++ src/components/BannerCelebrazioni.jsx
@@ -5,7 +5,7 @@ import logo from '../../public/images/logo.webp';
 import {MobileView, BrowserView} from 'react-device-detect';
 import {Modal, Button} from 'antd';
 
-const BannerCelebrazioni = () => {
+const BannerAlloggi = () => {
   const [isModalOpen, setIsModalOpen] = useState (false);
 
   const showModal = () => {
@@ -20,7 +20,7 @@ const BannerCelebrazioni = () => {
     setIsModalOpen (false);
   };
   return (
-    <div>
+    <div className='text-center'>
       <BrowserView>
         <div className="w-100 px-5 py-56 text-center bg-linear-to-r from-amber-300 to-amber-50 h-full border-l-4 border-t-4 border-b-4 border-black">
           <div className="flex flex-row my-3">
@@ -28,10 +28,20 @@ const BannerCelebrazioni = () => {
             <Image src={'/images/name.webp'} alt="" width={100} height={100} />
           </div>
           <div>
+            <h1>
+              Ospite, c'è un'esperienza che ti attende.
+            </h1>
             <br />
-            <h1> Celebrazioni.</h1>
+            <h3>
+              Scorri per scoprire come ogni dettaglio può essere personalizzato e scegli il capitolo da cui iniziare la tua storia con noi.
+            </h3>
+            <br />
+            <h1> Alloggi.</h1>
             <br /><h3>
-             Ogni grande momento merita un luogo che sappia custodirlo con cura. Matrimoni, anniversari o feste speciali: a Corte dei Semplici ogni evento prende vita intorno a te.
+              Le nostre camere e suite sono un rifugio di tranquillità,
+              dove il calore del legno antico si fonde
+              con l'eleganza discreta di un ambiente
+              curato nei minimi dettagli.
             </h3>
           </div>
           <Button type="primary">Inizia da qui</Button>
@@ -39,16 +49,17 @@ const BannerCelebrazioni = () => {
       </BrowserView>
       <MobileView>
         <>
-    <div className='text-center my-12'> <Button className='text-center' type="primary" onClick={showModal}>
-       Inizia da qui
-      </Button></div> 
-      <Modal
+       <div className='text-center mt-[1000vh] md:ml-0 ml-[150%]'> <Button className='text-center' type="primary" onClick={showModal}>
+        Inizia da qui
+       </Button>
+        </div> 
+        <Modal
         title="Basic Modal"
         closable={{ 'aria-label': 'Custom Close Button' }}
         open={isModalOpen}
         onOk={handleOk}
         onCancel={handleCancel}
-      >
+        >
         <div className="w-fit py-56 text-center bg-linear-to-r from-amber-300 to-amber-50 h-full border-4 border-black">
           <div className="flex flex-row my-3">
             <Image src={logo} alt="" width={100} height={100} />
@@ -63,22 +74,18 @@ const BannerCelebrazioni = () => {
               Scorri per scoprire come ogni dettaglio può essere personalizzato e scegli il capitolo da cui iniziare la tua storia con noi.
             </h3>
             <br />
-            <h1> Ospitalità: il viaggio che ti aspetta.</h1>
+            <h1>Alloggi.</h1>
             <br /><h3>
-              Corte dei Semplici nasce dall'amore per l'accoglienza,
-              la natura e il tempo di qualità.
-              Qui ogni dettaglio è pensato per risvegliare i tuoi sensi,
-              nutrire il presente e lasciare un segno indelebile
-              nella tua memoria.
+             Le nostre camere e suite sono un rifugio di tranquillità, dove il calore del legno antico si fonde con l'eleganza discreta di un ambiente curato nei minimi dettagli.
             </h3>
           </div>
           <Button className="" type="primary">Inizia da qui</Button>
         </div>
       </Modal>
-    </>
+      </>
       </MobileView>
     </div>
   );
 };
 
-export default BannerCelebrazioni;
+export default BannerAlloggi;
