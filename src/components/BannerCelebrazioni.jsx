fa52e87a45bec05fc3ea95765efd10ad173cdb8e
peppeferd <giuseppeferlazzo@outlook.it>
2025-08-27 09:15:39 +0200

    Commit

--- src/components/BannerCelebrazioni.jsx
+++ src/components/BannerCelebrazioni.jsx
@@ -5,7 +5,7 @@ import logo from '../../public/images/logo.webp';
 import {MobileView, BrowserView} from 'react-device-detect';
 import {Modal, Button} from 'antd';
 
-const BannerOspitalità = () => {
+const BannerCelebrazioni = () => {
   const [isModalOpen, setIsModalOpen] = useState (false);
 
   const showModal = () => {
@@ -28,21 +28,10 @@ const BannerOspitalità = () => {
             <Image src={'/images/name.webp'} alt="" width={100} height={100} />
           </div>
           <div>
-            <h1>
-              Ospite, c'è un'esperienza che ti attende.
-            </h1>
             <br />
-            <h3>
-              Scorri per scoprire come ogni dettaglio può essere personalizzato e scegli il capitolo da cui iniziare la tua storia con noi.
-            </h3>
-            <br />
-            <h1> Ospitalità: il viaggio che ti aspetta.</h1>
+            <h1> Celebrazioni.</h1>
             <br /><h3>
-              Corte dei Semplici nasce dall'amore per l'accoglienza,
-              la natura e il tempo di qualità.
-              Qui ogni dettaglio è pensato per risvegliare i tuoi sensi,
-              nutrire il presente e lasciare un segno indelebile
-              nella tua memoria.
+             Ogni grande momento merita un luogo che sappia custodirlo con cura. Matrimoni, anniversari o feste speciali: a Corte dei Semplici ogni evento prende vita intorno a te.
             </h3>
           </div>
           <Button type="primary">Inizia da qui</Button>
@@ -50,7 +39,7 @@ const BannerOspitalità = () => {
       </BrowserView>
       <MobileView>
         <>
-    <div className='text-center my-12'> <Button className='text-center' type="primary" onClick={showModal}>
+    <div className='text-center my-64'> <Button className='text-center' type="primary" onClick={showModal}>
        Inizia da qui
       </Button></div> 
       <Modal
@@ -60,7 +49,7 @@ const BannerOspitalità = () => {
         onOk={handleOk}
         onCancel={handleCancel}
       >
-        <div className="w-fit py-56 text-center bg-linear-to-r from-amber-300 to-amber-50 h-full border-4 border-black">
+        <div className="w-fit py-56 text-center bg-linear-to-r from-amber-300 to-amber-50 h-full border-l-4 border-t-4 border-b-4 border-black">
           <div className="flex flex-row my-3">
             <Image src={logo} alt="" width={100} height={100} />
             <Image src={'/images/name.webp'} alt="" width={100} height={100} />
@@ -92,4 +81,4 @@ const BannerOspitalità = () => {
   );
 };
 
-export default BannerOspitalità;
+export default BannerCelebrazioni;
