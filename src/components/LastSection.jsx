2f9382409dad05f2456cc8d777fc137422f02a43
peppeferd <giuseppeferlazzo@outlook.it>
2025-08-27 09:08:06 +0200

    Commit

--- src/components/LastSection.jsx
+++ src/components/LastSection.jsx
@@ -25,10 +25,7 @@ export default function LastSection () {
   }, []);
 
   return (
-    <main
-      ref={container}
-      className="relative max-h-[40%] mt-[100vh] mb-[100vh]"
-    >
+    <main ref={container} className="relative max-h-[40%] mb-50">
       <Section1 scrollYProgress={scrollYProgress} />
       <Section2 scrollYProgress={scrollYProgress} />
     </main>
