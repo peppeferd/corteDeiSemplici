8e536d4bd9d4c4398a7a483ffd218a0df8182a8f
peppeferd <giuseppeferlazzo@outlook.it>
2025-08-27 13:19:01 +0200

    New

--- src/components/BannerCelebrazioni.jsx
+++ src/components/BannerCelebrazioni.jsx
@@ -7,17 +7,32 @@ import {Modal, Button} from 'antd';
 
 const BannerCelebrazioni = () => {
   const [isModalOpen, setIsModalOpen] = useState (false);
-
+const [open, setOpen] = useState(false);
+  const [confirmLoading, setConfirmLoading] = useState(false);
+  const [modalText, setModalText] = useState('Content of the modal');
+  
+  const handleOk = () => {
+    setModalText('The modal will be closed after two seconds');
+    setConfirmLoading(true);
+    setTimeout(() => {
+      setOpen(false);
+      setConfirmLoading(false);
+    }, 2000);
+  };
   const showModal = () => {
     setIsModalOpen (true);
   };
 
-  const handleOk = () => {
+  const handleOk1 = () => {
     setIsModalOpen (false);
   };
 
-  const handleCancel = () => {
+  const handleCancel1 = () => {
     setIsModalOpen (false);
+  };
+   const handleCancel = () => {
+    console.log('Clicked cancel button');
+    setOpen(false);
   };
   return (
     <div>
@@ -39,30 +54,17 @@ const BannerCelebrazioni = () => {
       </BrowserView>
       <MobileView>
         <>
-    <div className='text-center my-12'> <Button className='text-center' type="primary" onClick={showModal}>
-       Inizia da qui
-      </Button></div> 
+      <Button type="primary" onClick={showModal}>
+        Open Modal with async logic
+      </Button>
       <Modal
-        title="Celebrazioni"
-        closable={{ 'aria-label': 'Custom Close Button' }}
-        open={isModalOpen}
+        title="Title"
+        open={open}
         onOk={handleOk}
+        confirmLoading={confirmLoading}
         onCancel={handleCancel}
       >
-        <div className="w-fit py-56 text-center bg-linear-to-r from-amber-300 to-amber-50 h-full border-l-4 border-t-4 border-b-4 border-black">
-          <div className="flex flex-row my-3">
-            <Image src={logo} alt="" width={100} height={100} />
-            <Image src={'/images/name.webp'} alt="" width={100} height={100} />
-          </div>
-           <div>
-            <br />
-            <h1> Celebrazioni.</h1>
-            <br /><h3>
-             Ogni grande momento merita un luogo che sappia custodirlo con cura. Matrimoni, anniversari o feste speciali: a Corte dei Semplici ogni evento prende vita intorno a te.
-            </h3>
-          </div>
-          <Button className="" type="primary">Inizia da qui</Button>
-        </div>
+        <p>{modalText}</p>
       </Modal>
     </>
       </MobileView>
